0f82dd2a4e2d59e955321133603d0d75327e4da7
Lily-Saxophone <riryharry0314@gmail.com>
2024-02-12 11:51:55 +0900

    エディタ作りかけ

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,29 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import Greet from "./components/Greet.vue";
+// import Greet from "./components/Greet.vue";
+import { appWindow } from "@tauri-apps/api/window";
+import { onBeforeMount, ref } from 'vue';
+import { useEditor, EditorContent } from '@tiptap/vue-3'
+import StarterKit from '@tiptap/starter-kit';
+import { getText } from './scripts/novel.js';
+
+const content = ref(getText());
+
+const editor = useEditor({
+  content: content,
+  extensions: [
+    StarterKit,
+  ],
+});
+
+onBeforeMount(async () => {
+  await appWindow.center();
+  await appWindow.setFocus();
+  // await appWindow.maximize();
+})
 </script>
 
 <template>
-  <div class="container">
+  <!-- <div class="container">
     <h1>Welcome to Tauri!</h1>
 
     <div class="row">
@@ -28,25 +46,67 @@ import Greet from "./components/Greet.vue";
       +
       <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
       +
-      <a href="https://github.com/tauri-apps/tauri-vscode" target="_blank"
-        >Tauri</a
-      >
+      <a href="https://github.com/tauri-apps/tauri-vscode" target="_blank">Tauri</a>
       +
-      <a href="https://github.com/rust-lang/rust-analyzer" target="_blank"
-        >rust-analyzer</a
-      >
+      <a href="https://github.com/rust-lang/rust-analyzer" target="_blank">rust-analyzer</a>
     </p>
 
     <Greet />
-  </div>
+  </div> -->
+  <v-app>
+    <v-main>
+      <tiptap />
+      <v-container>
+        <!-- <Editor v-model="content" /> -->
+        <div class="vertical-text" contenteditable="true">
+          <!-- <editor-content :editor="editor" /> -->
+          <p>{{ content }}</p>
+        </div>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <style scoped>
-.logo.vite:hover {
+/* .logo.vite:hover {
   filter: drop-shadow(0 0 2em #747bff);
 }
 
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #249b73);
+} */
+
+
+
+.vertical-text {
+  height: 100%;
+  width: 100%;
+  -webkit-writing-mode: vertical-rl;
+  -ms-writing-mode: tb-rl;
+  writing-mode: vertical-rl;
+  font-size: 20px;
+  font-family: "Noto Serif JP", serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-feature-settings: "palt";
+  font-feature-settings: "palt";
+  color: #2c3e50;
+  line-height: 2em;
+  text-indent: 1em;
+  vertical-align: "baseline";
+  outline: none;
+  overflow-x: scroll;
+  /* text-combine-upright: all; */
+}
+
+.container {
+  height: 100vh;
+  width: 100vw;
+  /* background: url(./assets/paper.jpg) repeat;
+  background-blend-mode: lighten; */
+}
+
+.box::-webkit-scrollbar {
+  display: none;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
 import { createApp } from "vue";
 import "./styles.css";
 import App from "./App.vue";
+// import router from "./router";
+import vuetify from "./plugins/vuetify";
 
-createApp(App).mount("#app");
+const app = createApp(App);
+
+// app.use(router)
+app.use(vuetify).mount("#app");
+
+// createApp(App).mount("#app");
